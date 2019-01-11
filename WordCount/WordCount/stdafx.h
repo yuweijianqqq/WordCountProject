0aa7547574d91951d4ab21b182e2f9a2cbfa1b62
Zealyoung <45253106+zealyoung@users.noreply.github.com>
2019-01-11 12:36:41 +0800

    Create stdafx.h

---
+++ WordCount/WordCount/stdafx.h
@@ -0,0 +1,17 @@
+// 定义所有库文件调用
+
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <stdlib.h>
+#include <io.h>
+#include <windows.h>
+#include <tchar.h>
+#include <atlstr.h>
+
+using namespace std;
+
+#include "define.h"
+#include "fundamental.h"
+#include "extended.h"
+
